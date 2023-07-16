cb009efe2bda1cda1b3a4b4329791dd50f2175e4
arcadio <c4net.7@gmail.com>
2023-07-16 05:28:20 +0330

    set structure of unit test

--- sum.test.ts
+++ sum.test.ts
@@ -3,11 +3,15 @@ import sum from "./sum";
 
 describe("sum module", () => {
   test("adds 1 + 2 to equal 3", () => {
+    //Arrange
+    const expected = 3;
     const acttual_1_ = 1;
     const acttual_2 = 2;
-    const sut = sum(acttual_1_, acttual_2);
-    const expected = 3;
 
+    //Act
+    const sut = sum(acttual_1_, acttual_2);
+
+    //Assert
     expect(sut).toBe(expected);
   });
 });
